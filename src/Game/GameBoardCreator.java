9f6b2a4cea62ab14a34df3f48f8a59391a306817
Quaade94 <larsquaadeconnect@gmail.com>
2016-01-04 15:34:54 +0100

    Alle fields som ikke kan købes kan nu oversættes i vores bundle!

--- src/Game/GameBoardCreator.java
+++ src/Game/GameBoardCreator.java
@@ -10,12 +10,38 @@ public class GameBoardCreator {
 	private int ArrayNr = 0;
 	
 	public void setGameBoard(){
-
+		
 		while (ArrayNr<=5){
+		//Try your luck field
 			GUI.setSubText(TryLuckNr[ArrayNr],Language.getLang("LUCK") );
 			GUI.setDescriptionText(TryLuckNr[ArrayNr], Language.getLang("CARD"));
 			ArrayNr++;
 		}
-	}
+		//Go to jail field
+		GUI.setSubText(31, Language.getLang("PRISONS"));
+		GUI.setDescriptionText(31, Language.getLang("PRISOND"));
+		
+		//Visit jail field
+		GUI.setSubText(11, Language.getLang("PRISON"));
+		GUI.setDescriptionText(11, Language.getLang("PRISONV"));
+		
+		//Start field
+		GUI.setSubText(1, Language.getLang("STARTS"));
+		GUI.setDescriptionText(1, Language.getLang("STARTD"));
+		
+		//Parking
+		GUI.setSubText(21, Language.getLang("PARKS"));
+		GUI.setDescriptionText(21, Language.getLang("PARKD"));
+		GUI.setTitleText(21, Language.getLang("PARKT"));
 
+		//Tax1 before START
+		GUI.setSubText(39, Language.getLang("TAX1S"));
+		GUI.setDescriptionText(39, Language.getLang("TAX1D"));
+		GUI.setTitleText(39, Language.getLang("TAX1T"));
+		
+		//Tax2 after START
+		GUI.setSubText(5, Language.getLang("TAX2S"));
+		GUI.setDescriptionText(5, Language.getLang("TAX2D"));
+		GUI.setTitleText(5, Language.getLang("TAX2T"));
+	}
 }
