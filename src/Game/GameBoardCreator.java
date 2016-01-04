75a6b8e73b58e21d0334f5b08918da1e30f76041
Quaade94 <larsquaadeconnect@gmail.com>
2016-01-04 12:24:36 +0100

    Indsætter GUI og opretter en GameBoardCreator.

---
+++ src/Game/GameBoardCreator.java
@@ -0,0 +1,15 @@
+package Game;
+
+import desktop_resources.GUI;
+
+public class GameBoardCreator {
+
+	public void setGameBoard(){
+		
+		GUI.setTitleText(21, "Parking");
+		
+		
+		
+	}
+	
+}
--- src/Game/Main.java
+++ src/Game/Main.java
@@ -5,8 +5,13 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Language Language = new Language();
+		GameBoardCreator GameBoardCreator = new GameBoardCreator();
+		GameBoardCreator.setGameBoard();
 		
-System.out.print(Language.getLang("TAX1"));
+		
+		
+		
+		
 	}
 
 }
