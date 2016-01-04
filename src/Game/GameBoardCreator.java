c61d9073efb8ebd1ef9c4d5f8b792e395dba084d
Quaade94 <larsquaadeconnect@gmail.com>
2016-01-04 14:50:38 +0100

    Lækkekort fields kan nu nemt oversættes.

--- src/Game/GameBoardCreator.java
+++ src/Game/GameBoardCreator.java
@@ -4,15 +4,18 @@ import desktop_resources.GUI;
 
 public class GameBoardCreator {
 
-	public void setGameBoard(){
-		
-		//The Title of the field
-		GUI.setTitleText(21, "Parkering");
-		
-		
-		
-		
-		
-	}
+	Language Language = new Language();
+
+	private int[] TryLuckNr = {3,8,18,23,34,37};
+	private int ArrayNr = 0;
 	
+	public void setGameBoard(){
+
+		while (ArrayNr<=5){
+			GUI.setSubText(TryLuckNr[ArrayNr],Language.getLang("LUCK") );
+			GUI.setDescriptionText(TryLuckNr[ArrayNr], Language.getLang("CARD"));
+			ArrayNr++;
+		}
+	}
+
 }
