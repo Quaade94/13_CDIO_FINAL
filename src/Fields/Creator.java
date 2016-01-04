7f63b09dad37fdd03f0e86680bc9841bd6521bf6
magnusmisbat <magnusmisbat@gmail.com>
2016-01-04 12:34:30 +0100

    Forsøg på objektorienterede felter

---
+++ src/Fields/Creator.java
@@ -0,0 +1,50 @@
+package Fields;
+
+public class Creator {
+	
+	public void createFields(){
+		
+		createTerritory();
+		createFleet();
+		createLaborcamp();
+		
+		
+
+	}
+	
+	
+	@SuppressWarnings("unused")
+	private static void createTerritory(){
+		
+		Territory rødovrevej = new Territory();
+		Territory hvidovrevej = new Territory();
+		Territory valbylanggade = new Territory();
+		Territory roskildevej = new Territory();
+		Territory allegade = new Territory();
+		Territory frediriksbergalle = new Territory();
+		Territory glkongevej = new Territory();
+		
+	}
+	@SuppressWarnings("unused")
+	private static void createFleet(){
+		
+		Fleet rederi1 = new Fleet();
+		Fleet rederi2 = new Fleet();
+		Fleet rederi3 = new Fleet();
+		Fleet rederi4 = new Fleet();
+		
+	}
+	@SuppressWarnings("unused")
+	private static void createLaborcamp(){
+		
+		Laborcamp tuborg = new Laborcamp();
+		Laborcamp cola = new Laborcamp();
+		
+	}
+	
+	
+	
+	
+	
+
+}
