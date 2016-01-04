466454561efd3f42405c54ad3ce947f2a6dcb968
Quaade94 <larsquaadeconnect@gmail.com>
2016-01-04 14:16:43 +0100

    ingenting

--- src/Game/GameBoardCreator.java
+++ src/Game/GameBoardCreator.java
@@ -7,11 +7,7 @@ public class GameBoardCreator {
 	public void setGameBoard(){
 		
 		//The Title of the field
-		GUI.setTitleText(21, "1");
-		//The Description of the field
-		GUI.setDescriptionText(21, "2");
-		//The text shown on the board
-		GUI.setSubText(21, "3");
+		GUI.setTitleText(21, "Parkering");
 		
 		
 		
