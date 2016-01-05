e6aa080744d9a8b4a7417c5642359bc99864a95e
magnusmisbat <magnusmisbat@gmail.com>
2016-01-05 09:48:05 +0100

    Rodet rundt

--- src/Players/Creator.java
+++ src/Players/Creator.java
@@ -2,11 +2,23 @@ package Players;
 
 public class Creator {
 	
-	private int startingCapital = 30000;
+		
+		int startingCapital = 30000;
+		
+		
+		
+		
+		
+		
+		
+		
+		Player Player1 = new Player(startingCapital, null, null, null, null, null);
+		Player Player2 = new Player(startingCapital, null, null, null, null, null);
+		Player Player3 = new Player(startingCapital, null, null, null, null, null);
+		Player Player4 = new Player(startingCapital, null, null, null, null, null);
+
 	
-	Player Player1 = new Player(startingCapital, null, null, null, null, null);
-	Player Player2 = new Player(startingCapital, null, null, null, null, null);
-	Player Player3 = new Player(startingCapital, null, null, null, null, null);
+
 
 	
 
