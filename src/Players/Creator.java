609e61c16e172ad2cf505c1574d88ca5b506f16a
magnusmisbat <magnusmisbat@gmail.com>
2016-01-05 09:34:36 +0100

    Oprettet Spiller Objekter, og tilføjet Car Pakke

---
+++ src/Players/Creator.java
@@ -0,0 +1,13 @@
+package Players;
+
+public class Creator {
+	
+	private int startingCapital = 30000;
+	
+	Player Player1 = new Player(startingCapital, null, null, null, null, null);
+	Player Player2 = new Player(startingCapital, null, null, null, null, null);
+	Player Player3 = new Player(startingCapital, null, null, null, null, null);
+
+	
+
+}
--- src/Players/Player.java
+++ src/Players/Player.java
@@ -1,5 +1,29 @@
 package Players;
 
+import Cars.Car;
+import Fields.Fleet;
+import Fields.Laborcamp;
+import Fields.Territory;
+
 public class Player {
+	
+	private Territory TerrOwned;
+	private Fleet FleeOwned;
+	private Laborcamp LabOwned;
+	private String Name;
+	private int Balance;
+	private Car PlayerCar;
+	
+	public Player (int StartingCapitalC, String NameC, Car PlayerCarC, Territory TerrOwnedC, Fleet FleeOwnedC, Laborcamp LabOwnedC){
+		
+		TerrOwned = TerrOwnedC;
+		FleeOwned = FleeOwnedC;
+		LabOwned = LabOwnedC;
+		Name = NameC;
+		Balance = StartingCapitalC;
+		PlayerCar = PlayerCarC;
+		
+	}
+	
 
 }
