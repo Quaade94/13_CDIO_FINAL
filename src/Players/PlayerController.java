09e978837e5f86ab07702c9a1e36426a07b5dca6
Big Fat Llama <lukas@10.16.99.208>
2016-01-06 16:01:46 +0100

    Lavet tjek af spillernavn

--- src/Players/PlayerController.java
+++ src/Players/PlayerController.java
@@ -1,12 +1,14 @@
 package Players;
 
+import java.sql.Blob;
+
 import desktop_resources.GUI;
 
 public class PlayerController {
 	
 	private int playerAmount;
 	private Player[] players;
-	private boolean names;
+	private boolean Bleh;
 
 	
 	/**
@@ -17,9 +19,6 @@ public class PlayerController {
 		playerAmount = GUI.getUserInteger("How many players?", 2, 6);
 		players = new Player[playerAmount];
 		
-		for (int i = 0; i < playerAmount; i++){
-			players[i] = new Player();
-		}
 	}
 	
 
@@ -27,16 +26,35 @@ public class PlayerController {
 	public void createPlayers(){
 		
 		for (int x = 0; x < playerAmount; x++){
-			
-			String name = GUI.getUserString("Enter your name: ");
-			players[x].setName(name);
-//				if (players[x].getName().equals(players[x].getName()) ){
-//					name = GUI.getUserString("Du kan ikke hedde det samme: ");
-//			}
-			
-			
+			String name;
+			boolean nameExists = false;
+			do{
+				nameExists = false;
+				name = GUI.getUserString("Enter your name: ");
+				for(int i = 0; i < x; i++){
+					if(players[i].getName().equals(players[x].getName())){
+						nameExists = true;
+					GUI.getUserButtonPressed("You can't be named the same as someone else!", "Type new name");
+					}
+				}
+				if(players[x].getName() == null){
+					nameExists = true;
+					GUI.getUserButtonPressed("You have a name idiot", "Type new name");
+				}	
+				if(players[x].getName().length() < 3 && players[x].getName().length() > 17){	
+					nameExists = true;
+					GUI.getUserButtonPressed("Your name needs to be minimum 3 characters short and 17 characters long", "Type new name");
+				}
+		
+			}while(nameExists);
+
+			Player player = new Player();
+			player.setName(name);
+			players[x] = player;
 		}
 	}
-
+	private boolean checkName(b names){
+		boolean output = false;
+	}
 	
 }
