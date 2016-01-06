bd64806fa9824039e4440959ef9f2d231aabb8e3
magnusmisbat <magnusmisbat@gmail.com>
2016-01-06 13:27:18 +0100

    Ændret metoder til static og tilføjet getPrice

--- src/Fields/FieldController.java
+++ src/Fields/FieldController.java
@@ -4,69 +4,76 @@ import Players.Player;
 
 public class FieldController {
 	
-	Field[] fields;
+	static Field[] fields;
 	public void initializeFieldCreator(){
 		fields = Creator.createFields();
 	}
 	
-	public int getRent(int fieldIndex){
+	public static int getPrice(int fieldIndex){
+		int price = -1;
+		if (fields[fieldIndex] instanceof Ownable){
+			price = ((Ownable) fields[fieldIndex]).getPrice();
+		}
+		return price;	
+	}
+	public static int getRent(int fieldIndex){
 		int rent = -1;
 		if (fields[fieldIndex] instanceof Ownable){
 			rent = ((Ownable) fields[fieldIndex]).getRent();
 		}
 		return rent;	
 	}
-	public int getHousePrice(int fieldIndex){
+	public static int getHousePrice(int fieldIndex){
 		int rent = -1;
 		if (fields[fieldIndex] instanceof Ownable){
 			rent = ((Ownable) fields[fieldIndex]).getHousePrice();
 		}
 		return rent;	
 	}
-	public int getPledgingValue(int fieldIndex){
+	public static int getPledgingValue(int fieldIndex){
 		int rent = -1;
 		if (fields[fieldIndex] instanceof Ownable){
 			rent = ((Ownable) fields[fieldIndex]).getPledgingValue();
 		}
 		return rent;	
 	}
-	public int getFieldNumber(int fieldIndex){
+	public static int getFieldNumber(int fieldIndex){
 		int rent = -1;
 		if (fields[fieldIndex] instanceof Ownable){
 			rent = ((Ownable) fields[fieldIndex]).getFieldNumber();
 		}
 		return rent;	
 	}
-	public String getName(int fieldIndex){
+	public static String getName(int fieldIndex){
 		String name = null;
 		if (fields[fieldIndex] instanceof Ownable){
 			name = ((Ownable) fields[fieldIndex]).getName();
 		}
 		return name;	
 	}
-	public void setOwner(int fieldIndex, Player Owner){
+	public static void setOwner(int fieldIndex, Player Owner){
 		if (fields[fieldIndex] instanceof Ownable){
 			((Ownable) fields[fieldIndex]).setOwner(Owner);
 		}
 	}
-	public Player getOwner(int fieldIndex){
+	public static Player getOwner(int fieldIndex){
 		Player Owner = null;
 		if (fields[fieldIndex] instanceof Ownable){
 			Owner = ((Ownable) fields[fieldIndex]).getOwner();
 		}
 		return Owner;	
 	}
-	public void resetOwner(int fieldIndex){
+	public static void resetOwner(int fieldIndex){
 		if (fields[fieldIndex] instanceof Ownable){
 			((Ownable) fields[fieldIndex]).resetOwner();
 		}
 	}
-	public void setHouseAmount(int fieldIndex, int AmountOfHouses){
+	public static void setHouseAmount(int fieldIndex, int AmountOfHouses){
 		if (fields[fieldIndex] instanceof Ownable){
 			((Ownable) fields[fieldIndex]).setHouseAmount(AmountOfHouses);
 		}
 	}
-	public int getHouseAmount(int fieldIndex){
+	public static int getHouseAmount(int fieldIndex){
 		int AmountOfHouses = -1;
 		if (fields[fieldIndex] instanceof Ownable){
 			AmountOfHouses = ((Ownable) fields[fieldIndex]).getHouseAmount();
