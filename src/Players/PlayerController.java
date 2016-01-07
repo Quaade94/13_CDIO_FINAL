8e97f668f29a50e6e6598c52413440460e2448d9
Big Fat Llama <lukas@10.16.99.208>
2016-01-07 14:23:52 +0100

    Ændringer i messagebundle og playercontroller
    Tilføjet outprints til messagebundle og beskrivelser til
    playercontroller

--- src/Players/PlayerController.java
+++ src/Players/PlayerController.java
@@ -8,6 +8,8 @@ import desktop_tests.CarTest;
 import java.awt.Color;
 import desktop_codebehind.Car;
 
+import Game.Language;
+
 public class PlayerController {
 	
 	private int playerAmount;
@@ -20,11 +22,14 @@ public class PlayerController {
 	 * @param playerAmount Antallet af spillere
 	 */
 	public PlayerController(){
-		playerAmount = GUI.getUserInteger("How many players?", 2, 6);
+		playerAmount = GUI.getUserInteger(Language.getLang("PLAYERNO"), 2, 6);
 		players = new Player[playerAmount];
 		
 	}
 	
+	/**
+	 * Opretter spillerne (Spiller objekter, spillerbiler mm.)
+	 */
 	public void createPlayers(){
 		
 		Car[] cars = CarBuilder.getCar();
@@ -32,32 +37,36 @@ public class PlayerController {
 			String name;
 			boolean nameExists = false;
 			do{
+				//Checks if the playerss name follows a certain criteria
 				nameExists = false;
-				name = GUI.getUserString("Enter your name: ");
+				name = GUI.getUserString(Language.getLang("WHATSNAME"));
 				for(int i = 0; i < x; i++){
 					if(players[i].getName().equals(name)){
 						nameExists = true;
-					GUI.getUserButtonPressed("You can't be named the same as someone else!", "Type new name");
+					GUI.getUserButtonPressed(Language.getLang("NAMEERRORSAME"), Language.getLang("TYPENEW"));
 					}
 				}
 				if(name.length() < 3 || name.length() > 17){	
 					nameExists = true;
-					GUI.getUserButtonPressed("Your name needs to be minimum 3 characters short and 17 characters long", "Type new name");
+					GUI.getUserButtonPressed(Language.getLang("NAMEERRORSHORTLONG"), Language.getLang("TYPENEW"));
 				}
 		
 			}while(nameExists);
-
+			
+			//Creates the player and puts him into an array
 			Player player = new Player();
 			player.setName(name);
 			players[x] = player;
-			System.out.println("spiller: "+players[x].getName());
-			System.out.println("penge: "+players[x].getAccount().getBalance());
+			//Creates the player on the board
 			GUI.addPlayer(players[x].getName(), players[x].getAccount().getBalance(), cars[x]);
-			System.out.println(players[x].getPlace());
 			GUI.setCar(players[x].getPlace()+1, players[x].getName());
  		}
 	}
 	
+	/**
+	 * A method to get the playerarray for use in other classes
+	 * @return An array of players
+	 */
 	public static Player[] getPlayers(){
 		return players;
 	}	
