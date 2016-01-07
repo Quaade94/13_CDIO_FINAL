7a775c5555ef39c4f5d71c1709746c31dfe36959
William Wiberg <metronom_dk@hotmail.com>
2016-01-07 13:40:07 +0100

    Ændringer i Start klassen

--- src/Fields/Start.java
+++ src/Fields/Start.java
@@ -1,27 +1,35 @@
 package Fields;
 
+import Fields.Field;
+
 public class Start extends Field{
-	
-	private String name = "Start";
-	private int FieldNumber = 1;
-	
-	
+
+	private String name;
+	private int FieldNumber;
+
+	public Start (int Fieldnumber, String name){
+		
+		this.name = name;
+		this.FieldNumber = FieldNumber;
+	}
+
 
 	@Override
 	public void landOnField() {
-		
-		
+
+
 	}
 
+	@Override
 	public String getName() {
-		
-	return name;
+
+		return name;
 	}
 
 	@Override
 	public int getFieldNumber() {
-		
-	return FieldNumber;
+
+		return FieldNumber;
 	}
 
 }
