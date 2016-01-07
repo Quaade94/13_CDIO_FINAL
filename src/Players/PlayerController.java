e0ecc6e2a4dbb8d7bf5cc2d6293cf08b65d4cf65
Big Fat Llama <lukas@10.16.99.208>
2016-01-07 09:44:28 +0100

    Spillere virker nu :):):):):)

--- src/Players/PlayerController.java
+++ src/Players/PlayerController.java
@@ -32,16 +32,12 @@ public class PlayerController {
 				nameExists = false;
 				name = GUI.getUserString("Enter your name: ");
 				for(int i = 0; i < x; i++){
-					if(players[i].getName().equals(players[x].getName())){
+					if(players[i].getName().equals(name)){
 						nameExists = true;
 					GUI.getUserButtonPressed("You can't be named the same as someone else!", "Type new name");
 					}
 				}
-				if(players[x].getName() == null){
-					nameExists = true;
-					GUI.getUserButtonPressed("You have a name idiot", "Type new name");
-				}	
-				if(players[x].getName().length() < 3 && players[x].getName().length() > 17){	
+				if(name.length() < 3 || name.length() > 17){	
 					nameExists = true;
 					GUI.getUserButtonPressed("Your name needs to be minimum 3 characters short and 17 characters long", "Type new name");
 				}
@@ -51,10 +47,7 @@ public class PlayerController {
 			Player player = new Player();
 			player.setName(name);
 			players[x] = player;
+			System.out.println("spiller: "+players[x].getName());
 		}
 	}
-	private boolean checkName(b names){
-		boolean output = false;
-	}
-	
 }
