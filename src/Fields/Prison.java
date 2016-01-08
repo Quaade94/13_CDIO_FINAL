63c70d67e03db932c6c154866167ae9225a8e47e
SebbyG <seb.hop@hotmail.com>
2016-01-08 14:07:50 +0100

    Rettet i Prison klassen

--- src/Fields/Prison.java
+++ src/Fields/Prison.java
@@ -1,19 +1,22 @@
 package Fields;
 
 import Players.Player;
-import Game.TurnSwitcher;
+import Players.PlayerController;
 
 public class Prison extends Jail_Prison_Start{
+	private PlayerController playerController;
 		
 	public Prison(int FieldNumberC, String fieldName, String SubTextC){
 		FieldName = fieldName;
 		FieldNumber = FieldNumberC;
 		SubText = SubTextC;
+		
 	}
 	
 	@Override
 	public void landOnField(){
-		Player jailedPlayer = TurnSwitcher.getNextPlayer();
+		playerController = new PlayerController();
+		Player jailedPlayer = playerController.getNextPlayer();
 		jailedPlayer.setJail(true);
 	}
 	@Override
