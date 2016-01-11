329af3139a5242bba1b20bbe701ca1520006c79e
Quaade94 <larsquaadeconnect@gmail.com>
2016-01-11 13:16:07 +0100

    Biler har nu en motor :)

--- src/Game/GameController.java
+++ src/Game/GameController.java
@@ -42,6 +42,22 @@ public class GameController {
 			
 		}
 	}
+	
+	public static void movement(int startPos, int finishPos, String name){
+		int p1 = startPos;
+		while(p1 != finishPos){
+			GUI.removeCar(p1, name);
+			p1++;
+			GUI.setCar(p1, name);
+			try {
+			    Thread.sleep(100);                 //1000 milliseconds is one second.
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}
+		}
+		
+		return;
+	}
 
 	private void standardTurn() {
 		System.out.println("Spiller 1 ejer: "+fC.getOwner(16));
@@ -60,8 +76,9 @@ public class GameController {
 			currentPlayer.setPlace(newPosition);
 			System.out.println("Nye " + newPosition+1);
 			//Communicating with GUI
-			GUI.removeCar(playerPosition+1, currentPlayer.getName());
-			GUI.setCar(newPosition+1, currentPlayer.getName());
+			GameController.movement(playerPosition+1,newPosition+1,currentPlayer.getName());
+//			GUI.removeCar(playerPosition+1, currentPlayer.getName());
+//			GUI.setCar(newPosition+1, currentPlayer.getName());
 			GUI.showMessage(Language.getLang("ROLLED") + " " + die.getDiceSum());
 			//Interacting with the field
 			if(playerPosition != 0){
