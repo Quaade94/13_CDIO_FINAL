a201b83c277d8154744fcb6e547966b8ce5548c6
SebbyG <seb.hop@hotmail.com>
2016-01-12 13:07:15 +0100

    Arbejdet på at kunne købe huse

--- src/Game/GameController.java
+++ src/Game/GameController.java
@@ -87,36 +87,88 @@ public class GameController {
 			String[] territoryColours = currentPlayer.getTerColour();
 			int blue = 0;
 			int purple = 0;
-			int rest;
+			int orange = 0;
+			int green = 0;
+			int grey = 0;
+			int red = 0;
+			int white = 0;
+			int yellow = 0;
 			boolean buildable = false;
 			for (int i = 0; i < 8; i++){
-				rest = 0;
 				for (int j = 0; j < territoryColours.length; j++){
 					if (i == 0 && territoryColours[j] == "BLUE"){
 						blue++;
 					} else if (i == 1 && territoryColours[j] == "ORANGE"){
-						rest++;
+						orange++;
 					} else if (i == 2 && territoryColours[j] == "GREEN"){
-						rest++;
-					} else if (i == 3 && territoryColours[j] == "GRAY"){
-						rest++;
+						green++;
+					} else if (i == 3 && territoryColours[j] == "GREY"){
+						grey++;
 					} else if (i == 4 && territoryColours[j] == "RED"){
-						rest++;
+						red++;
 					} else if (i == 5 && territoryColours[j] == "WHITE"){
-						rest++;
+						white++;
 					} else if (i == 6 && territoryColours[j] == "YELLOW"){
-						rest++;
+						yellow++;
 					} else if (i == 7 && territoryColours[j] == "Purple"){
 						purple++;
 					}
 				}
-				if (blue == 2 || purple == 2 || rest == 3){
+				if (blue == 2 || purple == 2 || orange == 3 || green == 3 || grey == 3 || red == 3 || white == 3 || yellow == 3){
 					buildable = true;
 				}
-
+				
 			}
 			if (buildable){
-
+				String[] buildables;
+				int arrayLength = blue + orange + green + grey + red + white + yellow + purple;
+				buildables = new String[arrayLength];
+				
+					if (blue == 2){
+						buildables = buildablesArray(buildables, 1, 3, 0);
+					}
+					if (purple == 2){
+						buildables = buildablesArray(buildables, 37, 39, 0);
+					}
+					if (orange == 3){
+						buildables = buildablesArray(buildables, 6, 8, 9);
+					}
+					if (green == 3){
+						buildables = buildablesArray(buildables, 11, 13, 14);
+					}
+					if (grey == 3){
+						buildables = buildablesArray(buildables, 16, 18, 19);
+					}
+					if (red == 3){
+						buildables = buildablesArray(buildables, 21, 23, 24);
+					}
+					if (white == 3){
+						buildables = buildablesArray(buildables, 26, 28, 29);
+					}
+					if (yellow == 3){
+						buildables = buildablesArray(buildables, 31, 33, 34);
+					}
+					
+					String playerChoice = GUI.getUserSelection("Choose the property you want to buy house on", buildables);
+					int housePrice = 0;
+					int place = 0;
+					for (int i = 0; i < 39; i++){
+						if(fC.getName(i) == playerChoice){
+							fC.getFieldNumber(i);
+							housePrice = fC.getHousePrice(i);
+						}
+					}
+					if (housePrice <= currentPlayer.getAccount().getBalance()){
+						fC.setHouseAmount(place, fC.getHouseAmount(place));
+						if(fC.getHouseAmount(place) >= 5){
+							GUI.setHotel(place, true);
+						} else {
+							GUI.setHouses(place+1, fC.getHouseAmount(place));
+						}
+					} else {
+						GUI.showMessage("You can't afford any houses on this property");
+					}
+					
 			} else {
 				GUI.showMessage("You don't own any properties you can build houses on!");
 			}
@@ -250,5 +302,26 @@ public class GameController {
 		while(diceplacex1 == diceplacex2){diceplacex2 =(int)(Math.random()*3+4);}
 		while(diceplacey1 == diceplacey2){diceplacey2 =(int)(Math.random()*2+2);}
 		GUI.setDice(dice1, diceplacex1, diceplacey1, dice2, diceplacex2, diceplacey2);
+	}
+	public String[] buildablesArray(String[] array, int index1, int index2, int index3){
+		for (int i = 0; i < array.length; i++){
+			if(array[i] == null){
+				array[i] = fC.getName(index1);
+				break;
+			}
+		}
+		for (int i = 0; i < array.length; i++){
+			if(array[i] == null){
+				array[i] = fC.getName(index2);
+				break;
+			}
+		}
+		for (int i = 0; i < array.length; i++){
+			if(index3 != 0 && array[i] == null){
+				array[i] = fC.getName(index3);
+				break;
+			}
+		}
+		return array;
 	}
 }
