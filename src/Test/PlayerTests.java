196f5a4eb697f3744e8ac28bc0524c859782bd61
Big Fat Llama <lukas@10.16.99.208>
2016-01-13 09:18:25 +0100

    Opsætning til test

--- src/Test/PlayerTests.java
+++ src/Test/PlayerTests.java
@@ -4,12 +4,15 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import Game.GameController;
+
 public class PlayerTests {
 	
+	GameController gC = new GameController();
 	
-
 	@Test
-	public void test() {
+	public void playerTests() {
+		gC.runGame();
 		fail("Not yet implemented");
 	}
 
